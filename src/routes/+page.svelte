cfed8b9fa2350abd524ea09f55905776c232a7df
Armchair Traveller <sentient.sober.armchair@gmail.com>
2022-12-11 08:03:40 -0800

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Ab from '~icons/mdi/ab-testing'
+</script>
+
+<main>
+  <h1>Welcome to SvelteKit</h1>
+  <p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+  </p>
+
+  <Ab />
+</main>
